334794b554ad505600f430ac056f4f801bc4af3d
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2020-08-15 12:07:45 +0800

    Update serverinfo.js

--- command/INFO/serverinfo.js
+++ command/INFO/serverinfo.js
@@ -5,18 +5,13 @@ module.exports.run = async (bot, message, args) => {
       .setColor("BLUE")
       .setTitle("서버정보")
       .setThumbnail(message.guild.iconURL({ dynamic: true }))
-      .setAuthor(
-        `${message.guild.name} 서버정보`,
-        message.guild.iconURL({ dynamic: true })
-      )
+      .setAuthor(`${message.guild.name} 서버정보`,message.guild.iconURL({ dynamic: true }))
       .addField("**서버이름:**", `${message.guild.name}`, true)
       .addField("**서버 주인:**", `${message.guild.owner}`, true)
       .addField("**멤버수:**", `${message.guild.memberCount}`, true)
       .addField("**역할수:**", `${message.guild.roles.cache.size}`, true)
       .setFooter(`꿀꿀봇 |`, bot.user.displayAvatarURL());
     message.channel.send({ embed: sEmbed });
-  } catch (e) {
-    console.log(e);
   }
 };
 
