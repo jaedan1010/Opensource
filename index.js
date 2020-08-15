bc1b412a80df6071e41246997d44e6f45a727fba
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2020-08-15 12:03:03 +0800

    필요 없는 모듈 제거

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const db = require("quick.db");
 const reload = require("self-reload-json")
 const fs = require('fs')
 const config = require("./config.json")
